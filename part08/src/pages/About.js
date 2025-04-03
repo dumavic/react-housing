c9b56ce050cca0e9af5c5beb2dbaf1ba61f7ec28
Victor Nguyen <vicvngu@gmail.com>
2025-04-03 18:13:01 -0400

    update footer

--- part08/src/pages/About.js
+++ part08/src/pages/About.js
@@ -1,8 +1,5 @@
 import './about.css';
 
-
-import Footer from "../components/Footer.js";
-
 function About() {
     return (
 <div>
@@ -17,7 +14,6 @@ function About() {
         <p><b>Asian Barbell Association (ABA)</b> was officially created by Victor Nguyen as a school organization at University of South Carolina on January 24, 2024. The goal of ABA was to create a community that motivates and guides others in succeeding in their self-improvement, using fitness as a means of teaching. We want to create a community where anyone who's open-minded and wanting to better themselves can come and find some answers. We emphasizes on Asian culture and wanting to break the stereotype of Asians being "non-athletic". Through workshops, mentorship programs, and supportive group workouts, the ABA empowers individuals of all fitness levels to build strength, confidence, and community. Together, let's lift not just weights, but also our spirits and celebrate the vibrant diversity of our Asian heritage.</p>
     </div>
 </section>
-<Footer />
 </div>
 
     )
